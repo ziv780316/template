80d9ebb76d12b7aef2fe15cb3d6ff3fe3f6a37f3
ziv <north780316@gmail.com>
2021-02-12 14:58:09 +0800

    [Description] add env_init.sh
    [Commit Files]
    env_init.sh
    
    -------------------------------------------

---
+++ env_init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export DEVELOP_ROOT_DIR=/home/ziv/work1
+export PATH=${DEVELOP_ROOT_DIR}/bin:${PATH}
+export LD_LIBRARY_PATH=${DEVELOP_ROOT_DIR}/numerical_solver/matrix_solver/lib:${LD_LIBRARY_PATH}
